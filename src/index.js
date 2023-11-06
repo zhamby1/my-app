a936c9025ea7dcd57d0a67e022ccaa69438bc27b
zhamby1 <36927788+zhamby1@users.noreply.github.com>
2023-11-06 18:54:42 +0000

    fetch

--- src/index.js
+++ src/index.js
@@ -12,53 +12,20 @@ const root = ReactDOM.createRoot(el)
 
 
 // create a component
-function RegistrationForm() {
-   const [inputs, setInputs] = useState({});
 
-   function handleSubmit(event) {
-      event.preventDefault();
-      alert(`Username: ${inputs.username}\nEmail: ${inputs.email}`);
+async function FetchForecast() {
+   const url = "https://wp.zybooks.com/weather.php?zip=90210";
+   const response = await fetch(url);
+   if (response.ok) {
+      const result = await response.json();
+      console.log("Day 1 high is " + result.forecast[0].high);
    }
-
-   function handleChange(event) {
-      const name = event.target.name;
-      const value = event.target.value;
-      setInputs(values => ({...values, [name]: value}));
-   }
-
-   return (
-      <form onSubmit={handleSubmit}>
-         <p>
-            <label htmlFor="username">
-               Username?
-            </label>
-            <input 
-               type="text" 
-               id="username"
-               name="username"
-               value={inputs.username || ""}
-               onChange={handleChange} />
-         </p>
-         <p>
-            <label htmlFor="email">
-               Email?
-            </label>
-            <input 
-               type="text" 
-               id="email"
-               name="email"
-               value={inputs.email || ""}
-               onChange={handleChange} />
-         </p>
-         <input type="submit" value="Register" />
-      </form>
-   );
-}
+};
    
 
 function App() {
    return (
-      <RegistrationForm />
+      <FetchForecast />
    );
 }
 //show the component on screen
